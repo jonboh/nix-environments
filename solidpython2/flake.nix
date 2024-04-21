a171abe645ae2280269cfb68b4f32a28d2679e26
jonboh <jon.bosque.hernando@gmail.com>
2024-04-21 10:03:48 +0200

    expose solidpython2 package

--- solidpython2/flake.nix
+++ solidpython2/flake.nix
@@ -5,12 +5,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   # Defines the outputs provided by this flake.
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.x86_64-linux.default = import ./solidpython2.nix { inherit pkgs; };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.x86_64-linux.python310Packages.solidpython2 = import ./solidpython2.nix {inherit pkgs;};
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [
+        (pkgs.python310.withPackages
+          (ps:
+            with ps; [
+              ruff-lsp
+              autopep8
+              debugpy
+              setuptools
+              self.packages.x86_64-linux.python310Packages.solidpython2
+            ]))
+      ];
     };
+  };
 }
